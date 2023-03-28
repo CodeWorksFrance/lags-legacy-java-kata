4ef9ab658fbee517c81cec47ec3df72bed2da303
ToF- <cthibauttof@gmail.com>
2023-03-28 16:12:29 +0200

    added initial project

---
+++ src/test/java/org/lags/RentTest.java
@@ -0,0 +1,12 @@
+package org.lags;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RentTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
